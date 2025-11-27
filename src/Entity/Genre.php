0055d51062b47e77a6ecdb70cd85d745fb51e988
Bouvet <guenaelbouvet@gmail.com>
2025-11-27 16:37:14 +0100

    genre relié a jeu

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GenreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
@@ -15,6 +17,17 @@ class Genre
 
     #[ORM\Column(length: 255)]
     private ?string $libGenre = null;
+
+    /**
+     * @var Collection<int, Jeux>
+     */
+    #[ORM\OneToMany(targetEntity: Jeux::class, mappedBy: 'genre')]
+    private Collection $jeuxes;
+
+    public function __construct()
+    {
+        $this->jeuxes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Genre
     public function setLibGenre(string $libGenre): static
     {
         $this->libGenre = $libGenre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Jeux>
+     */
+    public function getJeuxes(): Collection
+    {
+        return $this->jeuxes;
+    }
+
+    public function addJeux(Jeux $jeux): static
+    {
+        if (!$this->jeuxes->contains($jeux)) {
+            $this->jeuxes->add($jeux);
+            $jeux->setGenre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJeux(Jeux $jeux): static
+    {
+        if ($this->jeuxes->removeElement($jeux)) {
+            // set the owning side to null (unless already changed)
+            if ($jeux->getGenre() === $this) {
+                $jeux->setGenre(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Jeux.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuxRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuxRepository::class)]
+class Jeux
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'jeuxes')]
+    private ?Genre $genre = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGenre(): ?Genre
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?Genre $genre): static
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+}
