e75dcf073fcb78753af15425bb461f09b282fa50
masdonte <poonic54@protonmail.com>
2025-12-09 13:26:45 +0100

    reprendre page 39

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,21 +1,52 @@
 <?php
-// src/Controller/AccueilController.php
+/**
+ * Contrôleur de la page d'accueil
+ * 
+ * Ce contrôleur gère :
+ * - L'affichage de la page d'accueil pour les utilisateurs connectés
+ * - La redirection vers la page de connexion pour les utilisateurs non connectés
+ *
+ * @package App\Controller
+ * @author Original
+ * @version 1.0
+ */
+
 namespace App\Controller;
-//use Symfony\Component\HttpFoundation\Response;
+
+// Dépendances
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 class AccueilController extends AbstractController
 {
+    /**
+     * Page d'accueil de l'application
+     * 
+     * @Route("/", name="accueil")
+     * @param SessionInterface $session Interface de gestion de session
+     * @return Response Vue appropriée selon l'état de connexion
+     */
     #[Route('/', name: 'accueil')]
-    public function index(SessionInterface $session)
+    public function index(SessionInterface $session): Response
     {
-        // si un utilisateur est connecté on affiche la page d'accueil
+        // Vérification de l'état de connexion de l'utilisateur
+        // Priorité au système Symfony Security
         if ($this->getUser()) {
-            return $this->render('accueil.html.twig');
+            // Utilisateur connecté via Symfony Security : affichage de la page d'accueil
+            return $this->render('accueil.html.twig', [
+                'menuActif' => 'Accueil',
+            ]);
+        } elseif ($session->has('idUtilisateur')) {
+            // Utilisateur connecté via l'ancien système de session : affichage de la page d'accueil
+            return $this->render('accueil.html.twig', [
+                'menuActif' => 'Accueil',
+            ]);
         } else {
-            // sinon on affiche la page de connexion
-            return $this->render('security/login.html.twig');
+            // Utilisateur non connecté : redirection vers la page de connexion Symfony
+            return $this->redirectToRoute('app_login');
         }
     }
 }
+?>
---
+++ src/Form/MembreType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MembreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'label' => 'Rôles'
+            ])
+            ->add('password')
+            ->add('nomMembre')
+            ->add('prenomMembre')
+            ->add('telMembre')
+            ->add('mailMembre')
+            ->add('rueMembre')
+            ->add('cpMembre')
+            ->add('vileMembre')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membre::class,
+        ]);
+    }
+}
