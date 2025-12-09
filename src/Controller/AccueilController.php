49c0381bfdb2ec7c70f67dfb3ed251f1f9895485
masdonte <poonic54@protonmail.com>
2025-12-09 10:07:04 +0100

    suppression du modèle inutile et redirge l'utilisateur si non connecté

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,15 +1,21 @@
 <?php
 // src/Controller/AccueilController.php
 namespace App\Controller;
-
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index()
+    public function index(SessionInterface $session)
     {
-        return $this->render('accueil.html.twig');
+        // si un utilisateur est connecté on affiche la page d'accueil
+        if ($this->getUser()) {
+            return $this->render('accueil.html.twig');
+        } else {
+            // sinon on affiche la page de connexion
+            return $this->render('security/login.html.twig');
+        }
     }
 }
